163dfd671addc4cdf5a053723840a546d2bfada0
Armen <mench256@gmail.com>
2024-02-20 23:15:49 +0200

    Updates

--- _app/immutable/chunks/paths.BhXgYUW6.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1tm4zx5)==null?void 0:s.base)??"/greek";var e;const a=((e=globalThis.__sveltekit_1tm4zx5)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.NHdMfDnv.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.OwhEB5Bm.js";export{t as start};
