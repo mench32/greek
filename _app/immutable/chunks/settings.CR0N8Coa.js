204e0abdfc0a3e181d4af0b70e07b9c17e1b5faf
Armen <mench256@gmail.com>
2024-02-29 08:39:02 +0200

    Updates

--- _app/immutable/chunks/settings.CR0N8Coa.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./index.Dxcuq5Lg.js";const e=t({showDigraphs:!1,showArticles:!1,showTranslates:!1,sound:!0});e.subscribe(s=>{localStorage.setItem("settings",JSON.stringify(s))});export{e as s};
